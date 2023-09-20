1ea946f08f3257bb466f6e23863c53a685ad2ea0
Alexander Medved <medved1991alexander@gmail.com>
2023-09-20 22:01:42 +0300

    Add Route

--- IpEditor/BaseStation.cs
+++ IpEditor/BaseStation.cs
@@ -2,26 +2,16 @@
 
 internal sealed class BaseStation
 {
-    public string Name { get; set; }
+    public string Name { get; }
+    public Route OAM { get; }
+    public Route S1C { get; }
+    public Route S1U { get; }
 
-    #region MME
-    public string NodeOAM { get; set; }
-    public string NextHopOAM { get; set; }
-    public string VlanOAM { get; set; }
-    public string MaskOAM { get; set; }
-    #endregion
-
-    #region S1-C
-    public string NodeS1C { get; set; }
-    public string NextHopS1C { get; set; }
-    public string VlanS1C { get; set; }
-    public string MaskS1C { get; set; }
-    #endregion
-
-    #region S1-U
-    public string NodeS1U { get; set; }
-    public string NextHopS1U { get; set; }
-    public string VlanS1U { get; set; }
-    public string MaskS1U { get; set; }
-    #endregion
+    public BaseStation(string name, Route oam, Route s1c, Route s1u)
+    {
+        Name = name;
+        OAM = oam;
+        S1C = s1c;
+        S1U = s1u;           
+    }
 }
